02719dd1afd2b1a763470d020244687e144c2476
admin <admin@DESKTOP-6VV5L9L>
2025-03-28 17:27:21 +0900

    서블릿.

---
+++ BoardWeb/src/main/java/com/yedam/web/MainServlet.java
@@ -0,0 +1,67 @@
+package com.yedam.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.yedam.common.DataSource;
+import com.yedam.dao.BoardDao;
+import com.yedam.vo.BoardVO;
+
+/*
+ * tomcat(WAS)의 규칙에 따라 작성.
+ * 1. url 패턴(ex : http://localhost/Board/welcome)
+ * 2. 서블릿클래스 (init() -> service() -> destroy())
+ */
+public class MainServlet extends HttpServlet {
+	// 생성자.
+	public MainServlet() {
+		System.out.println("MainServlet() 호출.");
+	}
+
+	// 서블릿의 생명주기(init)
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("최초호출 한번만 실행 init()메소드입니다.");
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		System.out.println("호출될때마다 실행 service()메소드입니다.");
+//		BoardDao dao = new BoardDao();
+//		List<BoardVO> list = dao.boardList();
+
+		SqlSessionFactory sqlSessionFactory = DataSource.getInstance();
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			List<BoardVO> list = session.selectList("com.yedam.mapper.BoardMapper.selectBoard");
+
+			PrintWriter out = resp.getWriter();
+			String html = "<h3>게시글목록</h3>";
+			html += "<table border='2'>";
+			html += "<thead><tr><th>글번호</th><th>제목</th><th>내용</th><th>작성자</th><th>작성일시</th></tr><thead>";
+			html += "<tbody>";
+			for (BoardVO board : list) {
+				html += "<tr><td>" + board.getBoardNo() + "</td>" + "<td>" + board.getTitle() + "</td>" + "<td>"
+						+ board.getContent() + "</td>" + "<td>" + board.getWriter() + "</td>" + "<td>"
+						+ board.getWriteDate() + "</td></tr>";
+			}
+			html += "</tbody></table>";
+			out.print(html);
+		} // end of try
+	} // end of service.
+
+	@Override
+	public void destroy() {
+		System.out.println("서버종료시 실행 destroy()메소드입니다.");
+	}
+}
---
+++ BoardWeb/src/test/java/com/yedam/test/Test.java
@@ -0,0 +1,42 @@
+package com.yedam.test;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.yedam.common.DataSource;
+import com.yedam.mapper.BoardMapper;
+import com.yedam.vo.BoardVO;
+
+public class Test {
+	public static void main(String[] args) {
+		SqlSessionFactory sqlSessionFactory = DataSource.getInstance();
+		BoardVO board = new BoardVO();
+		board.setTitle("매퍼테스트44");
+		board.setContent("매퍼를 활용한 입력테스트");
+		board.setWriter("newbie");
+		board.setBoardNo(8);
+		
+		
+		try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+			BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+			
+//			int r = sqlSession.update("com.yedam.mapper.BoardMapper.deleteBoard", board.getBoardNo());
+			int r = mapper.deleteBoard(board.getBoardNo());
+			if (r == 1) {
+				System.out.println("등록성공.");
+				sqlSession.commit();
+			} else {
+				System.out.println("등록실패.");
+			}
+//			List<BoardVO> list = sqlSession.selectList("com.yedam.mapper.BoardMapper.selectBoard");
+			List<BoardVO> list = mapper.selectBoard();
+			for(BoardVO brd : list) {
+				System.out.println(brd.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
